c4bc204781d9e356e694d183fe29f2c7d0634dd9
havy127 <huynhthihavy127@gmail.com>
2019-10-22 01:50:47 +0700

    index.html

--- main.js
+++ main.js
@@ -7,4 +7,15 @@ function drawChart() {
         ['Gym', 2],
         ['Sleep', 8]
     ]);
+
+    // Optional; add a title and set the width and height of the chart
+    var options = {
+        'title': 'My Average Day',
+        'width': 550,
+        'height': 400
+    };
+
+    // Display the chart inside the <div> element with id="piechart"
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    chart.draw(data, options);
 }
